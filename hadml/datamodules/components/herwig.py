54e66d35d2907dbf7b14eea526b7c5f0442175da
remilvus <remilvus@gmail.com>
2023-10-25 10:18:17 +0200

    Refactor

--- hadml/datamodules/components/herwig.py
+++ hadml/datamodules/components/herwig.py
@@ -106,78 +106,9 @@ class Herwig(LightningDataModule):
             x_truth:   target truth information with conditonal information
         """
         fname = pathlib.Path(self.hparams.data_dir).joinpath(self.hparams.fname)
-        skip_processing = False
-        processed_fnames = None
-        if self.hparams.cache_dir is not None:
-            processed_fnames = {
-                name: pathlib.Path(self.hparams.cache_dir).joinpath(self.hparams.fname + f'_{name}.npy')
-                for name in ['q1', 'q2', 'c', 'h1', 'h2', 'event_labels']
-            }
-            skip_processing = all([os.path.exists(f) for f in processed_fnames.values()])
-        if not skip_processing:
-            clusters = []
-            q1, q2, c, h1, h2 = [], [], [], [], []
+        cluster, event_labels, h1, h2, q1, q2 = self._load_dataset(fname)
 
-            event_labels = []
-            labels_tmp = []
-            with open(fname) as f:
-                for i, event_line in enumerate(f):
-                    if i % 100000 == 0:
-                        print(i)
-                    if i % 5000000 == 0 and i > 0:
-                        df = pd.DataFrame(clusters)
-                        q1.append(split_to_float(df[0]))
-                        q2.append(split_to_float(df[1]))
-                        c.append(split_to_float(df[2]))
-                        h1.append(split_to_float(df[3]))
-                        h2.append(split_to_float(df[4]))
-                        event_labels.append(np.array(labels_tmp, dtype=np.int32))
-                        clusters = []
-                        labels_tmp = []
-                        del df
-                    items = event_line.split("|")[:-1]
-                    added_items = [c.split(";")[:-1] for c in items if c[:2] != '88']
-                    clusters += added_items
-                    labels_tmp += [i] * len(added_items)
-
-            df = pd.DataFrame(clusters)
-            q1.append(split_to_float(df[0]))
-            q2.append(split_to_float(df[1]))
-            c.append(split_to_float(df[2]))
-            h1.append(split_to_float(df[3]))
-            h2.append(split_to_float(df[4]))
-            event_labels.append(np.array(labels_tmp, dtype=np.int32))
-            del clusters
-            del labels_tmp
-            del df
-            q1 = np.concatenate(q1, dtype=np.float32, axis=0)
-            q2 = np.concatenate(q2, dtype=np.float32, axis=0)
-            c = np.concatenate(c, dtype=np.float32, axis=0)
-            h1 = np.concatenate(h1, dtype=np.float32, axis=0)
-            h2 = np.concatenate(h2, dtype=np.float32, axis=0)
-            event_labels = np.concatenate(event_labels, dtype=np.int32, axis=0)
-
-            if processed_fnames is not None:
-                for name, array in zip(['q1', 'q2', 'c', 'h1', 'h2', 'event_labels'], [q1, q2, c, h1, h2, event_labels]):
-                    print(f"Saving {name}...")
-                    np.save(processed_fnames[name], array)
-        else:
-            print(f"Loading data arrays...")
-            q1 = np.load(processed_fnames['q1'])
-            q2 = np.load(processed_fnames['q2'])
-            c = np.load(processed_fnames['c'])
-            h1 = np.load(processed_fnames['h1'])
-            h2 = np.load(processed_fnames['h2'])
-            event_labels = np.load(processed_fnames['event_labels'])
-        print(f"q1 shape: {q1.shape}")
-        print(f"q2 shape: {q2.shape}")
-        print(f"c shape: {c.shape}")
-        print(f"h1 shape: {h1.shape}")
-        print(f"h2 shape: {h2.shape}")
-        print(f"event_labels shape: {event_labels.shape}")
-
-        cluster = c[:, [1, 2, 3, 4]]
-        del c
+        cluster = cluster[:, [1, 2, 3, 4]]
 
         h1_types = h1[:, [0]]
         h2_types = h2[:, [0]]
@@ -191,11 +122,9 @@ class Herwig(LightningDataModule):
 
         org_inputs = np.concatenate([cluster, q1, q2, h1, h2], axis=1)
         del cluster, q1, q2, h1, h2
-        mask = (np.isin(h1_types.reshape(-1), list(self.pids_to_ix.keys()))) & (np.isin(h2_types.reshape(-1), list(self.pids_to_ix.keys())))
-        event_labels = event_labels[mask]
-        org_inputs = org_inputs[mask]
-        h1_types = h1_types[mask]
-        h2_types = h2_types[mask]
+
+        event_labels, h1_types, h2_types, org_inputs = self._filter_unnamed_types(event_labels, h1_types, h2_types,
+                                                                                  org_inputs)
 
         num_tot_evts = len(org_inputs)
         num_asked_events = get_num_asked_events(
@@ -207,16 +136,7 @@ class Herwig(LightningDataModule):
 
         new_inputs = boost(org_inputs)
 
-        def get_angles(four_vector):
-            _, px, py, pz = [four_vector[:, idx] for idx in range(4)]
-            pT = np.sqrt(px**2 + py**2)
-            phi = np.arctan(px / py)
-            theta = np.arctan(pT / pz)
-            # phi = np.sign(px) * np.arcsin(py / pT) + (1 - np.sign(px)) * np.sign(py) * np.pi / 2
-            # theta = np.arcsin(pT / np.sqrt(pz**2 + pT**2))
-            return phi, theta
-
-        phi, theta = get_angles(new_inputs[:, -4:])
+        phi, theta = self._get_angles(new_inputs[:, -4:])
         theta = theta + math.pi * (theta < 0)
 
         true_hadron_angles = np.stack([phi, theta], axis=1)
@@ -225,7 +145,7 @@ class Herwig(LightningDataModule):
         true_hadron_angles = torch.from_numpy(true_hadron_angles.astype(np.float32))
         true_hadron_momenta = torch.from_numpy(org_inputs[:, -8:])
 
-        q_phi, q_theta = get_angles(new_inputs[:, 4:8])
+        q_phi, q_theta = self._get_angles(new_inputs[:, 4:8])
         q_momenta = np.stack([q_phi, q_theta], axis=1)
         cond_info = np.concatenate([org_inputs[:, :4], q_momenta], axis=1)
         # cond_info_prescaler = MinMaxScaler((-1, 1))
@@ -251,6 +171,100 @@ class Herwig(LightningDataModule):
         
         self.summarize()
         return dataset
+
+    def _filter_unnamed_types(self, event_labels, h1_types, h2_types, org_inputs):
+        mask = (np.isin(h1_types.reshape(-1), list(self.pids_to_ix.keys()))) & (
+            np.isin(h2_types.reshape(-1), list(self.pids_to_ix.keys())))
+        event_labels = event_labels[mask]
+        org_inputs = org_inputs[mask]
+        h1_types = h1_types[mask]
+        h2_types = h2_types[mask]
+        return event_labels, h1_types, h2_types, org_inputs
+
+    @staticmethod
+    def _get_angles(four_vector):
+        _, px, py, pz = [four_vector[:, idx] for idx in range(4)]
+        pT = np.sqrt(px ** 2 + py ** 2)
+        phi = np.arctan(px / py)
+        theta = np.arctan(pT / pz)
+        # phi = np.sign(px) * np.arcsin(py / pT) + (1 - np.sign(px)) * np.sign(py) * np.pi / 2
+        # theta = np.arcsin(pT / np.sqrt(pz**2 + pT**2))
+        return phi, theta
+
+    def _load_dataset(self, fname):
+        load_from_cache = False
+        processed_fnames = None
+        if self.hparams.cache_dir is not None:
+            processed_fnames = {
+                name: pathlib.Path(self.hparams.cache_dir).joinpath(self.hparams.fname + f'_{name}.npy')
+                for name in ['q1', 'q2', 'c', 'h1', 'h2', 'event_labels']
+            }
+            load_from_cache = all([os.path.exists(f) for f in processed_fnames.values()])
+        if not load_from_cache:
+            cluster, event_labels, h1, h2, q1, q2 = self._load_raw_dataset(fname)
+
+            if processed_fnames is not None:
+                self._save_data_arrays(event_labels, h1, h2, processed_fnames, q1, q2)
+        else:
+            print(f"Loading cached data arrays...")
+            cluster, event_labels, h1, h2, q1, q2 = self._load_data_arrays(processed_fnames)
+        return cluster, event_labels, h1, h2, q1, q2
+
+    def _load_data_arrays(self, processed_fnames):
+        q1 = np.load(processed_fnames['q1'])
+        q2 = np.load(processed_fnames['q2'])
+        cluster = np.load(processed_fnames['c'])
+        h1 = np.load(processed_fnames['h1'])
+        h2 = np.load(processed_fnames['h2'])
+        event_labels = np.load(processed_fnames['event_labels'])
+        return cluster, event_labels, h1, h2, q1, q2
+
+    def _save_data_arrays(self, event_labels, h1, h2, processed_fnames, q1, q2):
+        for name, array in zip(['q1', 'q2', 'c', 'h1', 'h2', 'event_labels'], [q1, q2, c, h1, h2, event_labels]):
+            print(f"Saving {name}...")
+            np.save(processed_fnames[name], array)
+
+    def _load_raw_dataset(self, fname):
+        clusters = []
+        q1, q2, c, h1, h2 = [], [], [], [], []
+        event_labels = []
+        labels_tmp = []
+        with open(fname) as f:
+            for i, event_line in enumerate(f):
+                if i % 100000 == 0:
+                    print(i)
+                if i % 5000000 == 0 and i > 0:
+                    df = pd.DataFrame(clusters)
+                    q1.append(split_to_float(df[0]))
+                    q2.append(split_to_float(df[1]))
+                    c.append(split_to_float(df[2]))
+                    h1.append(split_to_float(df[3]))
+                    h2.append(split_to_float(df[4]))
+                    event_labels.append(np.array(labels_tmp, dtype=np.int32))
+                    clusters = []
+                    labels_tmp = []
+                    del df
+                items = event_line.split("|")[:-1]
+                added_items = [c.split(";")[:-1] for c in items if c[:2] != '88']
+                clusters += added_items
+                labels_tmp += [i] * len(added_items)
+        df = pd.DataFrame(clusters)
+        q1.append(split_to_float(df[0]))
+        q2.append(split_to_float(df[1]))
+        c.append(split_to_float(df[2]))
+        h1.append(split_to_float(df[3]))
+        h2.append(split_to_float(df[4]))
+        event_labels.append(np.array(labels_tmp, dtype=np.int32))
+        del clusters
+        del labels_tmp
+        del df
+        q1 = np.concatenate(q1, dtype=np.float32, axis=0)
+        q2 = np.concatenate(q2, dtype=np.float32, axis=0)
+        c = np.concatenate(c, dtype=np.float32, axis=0)
+        h1 = np.concatenate(h1, dtype=np.float32, axis=0)
+        h2 = np.concatenate(h2, dtype=np.float32, axis=0)
+        event_labels = np.concatenate(event_labels, dtype=np.int32, axis=0)
+        return c, event_labels, h1, h2, q1, q2
 
     def summarize(self):
         print(f"Reading data from: {self.hparams.data_dir}")
